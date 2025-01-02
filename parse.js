c29552ce2f4e6ff000e60b022171fc56b46c124c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-01-02 09:24:26 +0000

    adding episodes list

--- parse.js
+++ parse.js
@@ -1,153 +1,205 @@
-const Parser = require('rss-parser');
-const fs = require('fs');
-const stringCleaner = require('./libs/stringCleaner');
+const Parser = require("rss-parser");
+const fs = require("fs");
+const stringCleaner = require("./libs/stringCleaner");
 
-const URL = 'https://www.spreaker.com/show/3039391/episodes/feed';
+const URL = "https://www.spreaker.com/show/3039391/episodes/feed";
 
 class Show {
-    constructor(title, description, link, feedUrl, copyright, image, author, email, language, explicit, podcasts) {
-        this.title = title;
-        this.description = description;
-        this.link = link;
-        this.feedUrl = feedUrl;
-        this.copyright = copyright;
-        this.image = image;
-        this.author = author;
-        this.email = email;
-        this.language = language;
-        this.explicit = explicit;
+  constructor(
+    title,
+    description,
+    link,
+    feedUrl,
+    copyright,
+    image,
+    author,
+    email,
+    language,
+    explicit,
+    podcasts
+  ) {
+    this.title = title;
+    this.description = description;
+    this.link = link;
+    this.feedUrl = feedUrl;
+    this.copyright = copyright;
+    this.image = image;
+    this.author = author;
+    this.email = email;
+    this.language = language;
+    this.explicit = explicit;
 
-        this.podcasts = podcasts;
+    this.podcasts = podcasts;
+  }
+
+  static fromFeed(data) {
+    let {
+      title,
+      description,
+      link,
+      feedUrl,
+      copyright,
+      language,
+      itunes,
+      items = [],
+    } = data;
+    description = stringCleaner.rmHtml(description);
+    const image = itunes.image;
+    const author = itunes.author || itunes.owner.name;
+    const email = itunes.owner.email;
+    const explicit = stringCleaner.booleanize(itunes.explicit);
+
+    let podcasts = items;
+
+    if (items && items.length) {
+      podcasts = items.map((item) => {
+        return Podcast.fromFeed(item).toJs();
+      });
     }
 
-    static fromFeed(data) {
-        let { title, description, link, feedUrl, copyright, language, itunes, items = [] } = data;
-        description = stringCleaner.rmHtml(description);
-        const image = itunes.image;
-        const author = itunes.author || itunes.owner.name;
-        const email = itunes.owner.email;
-        const explicit = stringCleaner.booleanize(itunes.explicit);
+    return new Show(
+      title,
+      description,
+      link,
+      feedUrl,
+      copyright,
+      image,
+      author,
+      email,
+      language,
+      explicit,
+      podcasts
+    );
+  }
 
-        let podcasts = items;
-
-        if (items && items.length) {
-            podcasts = items.map(item => {
-                return (Podcast.fromFeed(item)).toJs();
-            });
-        }
-
-        return new Show(title, description, link, feedUrl, copyright, image, author, email, language, explicit, podcasts);
-    }
-
-    toJs() {
-        return {
-            title: this.title,
-            description: this.description,
-            link: this.link,
-            feedUrl: this.feedUrl,
-            copyright: this.copyright,
-            image: this.image,
-            author: this.author,
-            email: this.email,
-            language: this.language,
-            explicit: this.explicit,
-            podcasts: this.podcasts
-        };
-    }
-
+  toJs() {
+    return {
+      title: this.title,
+      description: this.description,
+      link: this.link,
+      feedUrl: this.feedUrl,
+      copyright: this.copyright,
+      image: this.image,
+      author: this.author,
+      email: this.email,
+      language: this.language,
+      explicit: this.explicit,
+      podcasts: this.podcasts,
+    };
+  }
 }
 
 class Podcast {
+  constructor(title, pubDate, fileUrl, content, contentSnippet, guid, isoDate) {
+    this.title = title;
+    this.pubDate = pubDate;
+    this.fileUrl = fileUrl;
+    this.content = content;
+    this.contentSnippet = contentSnippet;
+    this.guid = guid;
+    this.isoDate = isoDate;
+  }
 
-    constructor(title, pubDate, fileUrl, content, contentSnippet, guid, isoDate) {
-        this.title = title;
-        this.pubDate = pubDate;
-        this.fileUrl = fileUrl;
-        this.content = content;
-        this.contentSnippet = contentSnippet;
-        this.guid = guid;
-        this.isoDate = isoDate;
-    }
+  static fromFeed(data) {
+    let { title, pubDate, enclosure, content, contentSnippet, guid, isoDate } =
+      data;
+    const fileUrl = enclosure.url;
+    content = stringCleaner.rmHtml(content);
+    contentSnippet = stringCleaner.rmHtml(contentSnippet);
 
-    static fromFeed(data) {
-        let { title, pubDate, enclosure, content, contentSnippet, guid, isoDate } = data;
-        const fileUrl = enclosure.url;
-        content = stringCleaner.rmHtml(content);
-        contentSnippet = stringCleaner.rmHtml(contentSnippet);
+    return new Podcast(
+      title,
+      pubDate,
+      fileUrl,
+      content,
+      contentSnippet,
+      guid,
+      isoDate
+    );
+  }
 
-        return new Podcast(title, pubDate, fileUrl, content, contentSnippet, guid, isoDate);
-    }
-
-    toJs() {
-        return {
-            title: this.title,
-            pubDate: this.pubDate,
-            fileUrl: this.fileUrl,
-            content: this.content,
-            contentSnippet: this.contentSnippet,
-            guid: this.guid,
-            isoDate: this.isoDate
-        };
-    }
-
+  toJs() {
+    return {
+      title: this.title,
+      pubDate: this.pubDate,
+      fileUrl: this.fileUrl,
+      content: this.content,
+      contentSnippet: this.contentSnippet,
+      guid: this.guid,
+      isoDate: this.isoDate,
+    };
+  }
 }
-
-
 
 const parser = new Parser();
 
-
-const feedParser = async url => {
-    const feed = await parser.parseURL(url);
-    return feed;
+const feedParser = async (url) => {
+  const feed = await parser.parseURL(url);
+  return feed;
 };
 
 const main = async () => {
-    console.log(`\t getting remote feed...`);
-    const data = await feedParser(URL);
-    const show = Show.fromFeed(data);
-    console.log(`\t parsing links...`);
+  console.log(`\t getting remote feed...`);
+  const data = await feedParser(URL);
+  const show = Show.fromFeed(data);
+  console.log(`\t parsing links...`);
 
-    save(show, 'shows');
-    const links = [];
-    for (const episode of show.podcasts) {
-        console.log(`\t\t- ${episode.title}`);
-        const rows = episode.content.replace(/\n+/gm, '\n').split('\n');
-        let previous = null;
-        for (const row of rows) {
-            if (stringCleaner.isValidURL(row) && previous) {
-                links.push({
-                    link: row,
-                    description: previous.replace("►", ""),
-                    title: episode.title,
-                    fileUrl: episode.fileUrl,
-                });
-                previous = null;
-            } else {
-                previous = row;
-            }
-        }
-        console.log('\t\t...done\n');
+  save(show, "shows");
+  const episodes = [];
+  const links = [];
+  for (const episode of show.podcasts) {
+    const currentEpisodeLinks = [];
+    console.log(`\t\t- ${episode.title}`);
+    const rows = episode.content.replace(/\n+/gm, "\n").split("\n");
+    let previous = null;
+    for (const row of rows) {
+      if (stringCleaner.isValidURL(row) && previous) {
+        const link = {
+          link: row,
+          description: previous.replace("►", ""),
+          title: episode.title,
+          fileUrl: episode.fileUrl,
+        };
+        currentEpisodeLinks.push(link);
+        links.push(link);
+        previous = null;
+      } else {
+        previous = row;
+      }
     }
+    episodes.push({
+      title: episode.title,
+      fileUrl: episode.fileUrl,
+      links: currentEpisodeLinks,
+    });
+    console.log("\t\t...done\n");
+  }
 
-    save(links, 'links', true);
-    console.log('...all done bye <3\n');
+  const linksFile = save(links, "links");
+  const episodesFile = save(episodes, "episodes");
+
+  for (const filename of [linksFile, episodesFile]) {
+    console.log(`\t moving ${filename} to public src folder...`);
+    fs.copyFileSync(`output/${filename}`, `public/${filename}`);
+  }
+  console.log(`\t generating consts`);
+  fs.writeFileSync(
+    `src/consts.js`,
+    `export const LINK_URL = '/${linksFile}';\nexport const EPISODES_URL = '/${episodesFile}';\nexport const LAST_UPDATE = new Date('${now()}');`
+  );
+
+  console.log("...all done bye <3\n");
 };
 
-const save = (data, prefix, toPublic = false) => {
-    const now = (new Date()).toISOString();
-    let filename = now.replace(/:/gm, '.');
-    filename = `${prefix}_${filename}.json`;
-    console.log(`\t saving ${filename} file...`);
-    fs.writeFileSync(`output/${filename}`, JSON.stringify(data));
+const now = () => new Date().toISOString();
 
-    if (toPublic) {
-        console.log(`\t moving ${filename} to public src folder...`);
-        fs.copyFileSync(`output/${filename}`, `public/${filename}`);
-        console.log(`\t generating consts`);
-        fs.writeFileSync(`src/consts.js`, `export const LINK_URL = '/${filename}';\nexport const LAST_UPDATE = new Date('${now}');`);
-    }
+const save = (data, prefix) => {
+  let filename = now().replace(/:/gm, ".");
+  filename = `${prefix}_${filename}.json`;
+  console.log(`\t saving ${filename} file...`);
+  fs.writeFileSync(`output/${filename}`, JSON.stringify(data));
+
+  return filename;
 };
-
 
 main();
--- src/main.js
+++ src/main.js
@@ -1,63 +1,95 @@
 import autoComplete from "@tarekraafat/autocomplete.js";
-import { LINK_URL, LAST_UPDATE } from './consts.js';
+import { LINK_URL, LAST_UPDATE, EPISODES_URL } from "./consts.js";
 
-const autoCompleteJS = new autoComplete({
+function attachLinkSearch() {
+  const autoCompleteJS = new autoComplete({
     placeHolder: "Cerca note 🍕...",
     data: {
-        src: async () => {
-            try {
-                document
-                    .getElementById("autoComplete")
-                    .setAttribute("placeholder", "Sto cercando 🍕...");
-                const source = await fetch(
-                    LINK_URL
-                );
-                const data = await source.json();
-                document
-                    .getElementById("autoComplete")
-                    .setAttribute("placeholder", autoCompleteJS.placeHolder);
-                return data;
-            } catch (error) {
-                return error;
-            }
-        },
-        keys: ["description"]
+      src: async () => {
+        try {
+          document
+            .getElementById("autoComplete")
+            .setAttribute("placeholder", "Sto cercando 🍕...");
+          const source = await fetch(LINK_URL);
+          const data = await source.json();
+          document
+            .getElementById("autoComplete")
+            .setAttribute("placeholder", autoCompleteJS.placeHolder);
+          return data;
+        } catch (error) {
+          return error;
+        }
+      },
+      keys: ["description"],
     },
     resultItem: {
-        highlight: {
-            render: true
-        }
+      highlight: {
+        render: true,
+      },
     },
     resultsList: {
-        element: (list, data) => {
-            if (!data.results.length) {
-                const message = document.createElement("div");
-                message.setAttribute("class", "no_result");
-                message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
-                list.prepend(message);
-            }
-        },
-        noResults: true,
+      element: (list, data) => {
+        if (!data.results.length) {
+          const message = document.createElement("div");
+          message.setAttribute("class", "no_result");
+          message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+          list.prepend(message);
+        }
+      },
+      noResults: true,
     },
-});
+  });
 
-autoCompleteJS.input.addEventListener("selection", function (event) {
+  autoCompleteJS.input.addEventListener("selection", function (event) {
     const result = event.detail;
     autoCompleteJS.input.blur();
     const selection = result.selection.value[result.selection.key];
     document.querySelector(".results").innerHTML = parseResult(result);
     autoCompleteJS.input.value = selection;
     console.log(result);
-});
+  });
+}
 
+function attachEpisodesList() {
+  fetch(EPISODES_URL)
+    .then((res) => res.json())
+    .then((eps) => {
+      const result = eps.map((e) => episodeAccordion(e));
+      document.querySelector(".results").innerHTML = result.join("\n");
+    });
+}
 
 function resultCard({ value }) {
-    return `<div class="result">
+  return `<div class="result">
     <h3>${value.title}</h3>
     <a href="${value.fileUrl}" target="_blank">Play 🔊</a>
     <a href="${value.link}" target="_blank">${value.description} 🔗</a>
     </div>`;
+}
 
+function episodeAccordion(episode) {
+  return `<details name="episodes">
+  <summary>${episode.title}</summary>
+  <div class="episodeInfo">
+    <a href="${episode.fileUrl}" target="_blank">Ascolta 🔉</a>
+  </div>
+  ${
+    episode.links && episode.links.length > 0
+      ? `<ul>
+  ${episode.links
+    .map(
+      (l) => `<li><a href="${l.link}" target="_blank">${l.description}</a></li>`
+    )
+    .join("\n")}
+  </ul>`
+      : ""
+  }
+  ${
+    episode.links && episode.links.length < 1
+      ? `<h2> No links in this episode</h2>`
+      : ""
+  }
+  </details>`;
 }
 
 /*
@@ -68,21 +100,25 @@ function resultCard({ value }) {
   },
 */
 function parseResult(result) {
-    let template = `${resultCard(result.selection)}`;
+  let template = `${resultCard(result.selection)}`;
 
-    if (result.results.length < 2) {
-        return template;
-    }
-
-    template += '<h2>Altri Risultati</h2>';
-    for (let match of result.results) {
-        template += resultCard(match);
-    }
-
+  if (result.results.length < 2) {
     return template;
+  }
+
+  template += "<h2>Altri Risultati</h2>";
+  for (let match of result.results) {
+    template += resultCard(match);
+  }
+
+  return template;
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".lastUpdate").innerHTML = `Aggiornato: ${LAST_UPDATE.toLocaleString()}`;
+  document.location.href.includes("episodes")
+    ? attachEpisodesList()
+    : attachLinkSearch();
+  document.querySelector(
+    ".lastUpdate"
+  ).innerHTML = `Aggiornato: ${LAST_UPDATE.toLocaleString()}`;
 });
