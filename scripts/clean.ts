141d0d80c7c9431204ad61a63ba593a82f8e2dd9
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-01-20 20:02:43 +0000

    time in the footer?

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -1,6 +1,14 @@
 import { $ } from "bun";
 import { OUTPUT_DIR, PUBLIC_DATA_DIR } from "./config";
+async function tryDo(func: () => void) {
+  try {
+    await func();
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-await $`rm ${OUTPUT_DIR}/*.json`;
-await $`rm ${PUBLIC_DATA_DIR}/*.json`;
-await $`rm .env`;
+tryDo(async () => await $`ls ${OUTPUT_DIR}/*.json`);
+tryDo(async () => await $`rm ${OUTPUT_DIR}/*.json`);
+tryDo(async () => await $`rm ${PUBLIC_DATA_DIR}/*.json`);
+tryDo(async () => await $`rm .env`);
