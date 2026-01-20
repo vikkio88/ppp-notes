fa39f361b8ef3267a2a834a590e390a4b987b797
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2026-01-20 19:50:02 +0000

    adding security todos

--- src/components/api/episode.ts
+++ src/components/api/episode.ts
@@ -1,6 +1,7 @@
 import type { Episode } from "../dataEntry/libs/types";
 
 export async function load(uri: string): Promise<Episode | null> {
+  //TODO: check URI is same as current, and sanitize episode before inputting in
   const resp = await fetch(uri);
   if (!resp.ok) {
     console.error(`Could not load '${uri}'`);
